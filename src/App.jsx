c580a16371f951f7e4c1e6880908bd5978a4bb98
Erik Truckner <eriktruckner@comcast.net>
2023-03-03 07:11:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Box from './Box'
+
+
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-0.8, 0, 0]} />
+      <Box position={[0.8, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
+
+console.log(Canvas);
